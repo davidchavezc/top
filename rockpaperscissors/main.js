7c53ea67db1301a57f01650c6a1bfe162a1b0e7c
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2024-12-20 10:50:53 -0600

    Add messages into main.js

--- rockpaperscissors/main.js
+++ rockpaperscissors/main.js
@@ -1,6 +1,6 @@
 function getComputerChoice(){
     let computerChoice = parseInt(Math.random() * 3) + 1; // Generates a random number between 1 and 3
-    console.log(computerChoice)
+    // console.log(computerChoice)
     return computerChoice;
 }
 let userScore = 0;
@@ -13,6 +13,72 @@ const buttons = document.querySelectorAll("button");
 buttons.forEach((button) =>  {
     button.addEventListener('click', () => {
         choice = button.textContent.toLowerCase();
-        console.log(choice)
+        rivalChoice = getComputerChoice();
+        playRound(choice, rivalChoice);
     })
 });
+
+const scoreKeeper = document.getElementById('#score');
+const winnerKeeper = document.getElementById('#ganador');
+
+let playMessage = [
+    'Empate!!',
+    'El papel del rival envuelve a tu roca!!',
+    '¡¡La tijera del rival es destrozada por tu roca!!',
+    '¡¡Tu papel envuelve la roca del rival!!',
+    '¡¡La tijera del rival corta tu papel!!',
+    '¡¡Tu roca destroza la tijera del rival!!',
+    '¡¡Tu tijera corta el papel del rival!!',
+    '¡¡La roca rival destruye tus tijeras!!'
+ ]
+
+ function playRound(choice, rivalChoice){
+    let winner;
+    switch(choice){
+        case 'roca':
+            if(rivalChoice===1){ // roca
+                winMessage = playMessage[0];
+                // console.log(winMessage)
+            }
+            if(rivalChoice===2){ // papel
+                winMessage = playMessage[1];
+                winner = 'rival';
+            }
+            if(rivalChoice===3){ // tijera
+                winMessage = playMessage[2];
+                winner = 'user';
+            }
+            break;
+        case 'papel':
+            if(rivalChoice===1){
+                winMessage = playMessage[3];
+                winner = 'user';
+                break;
+            }
+            if(rivalChoice===2){
+                winMessage = playMessage[0];
+                break;
+            }
+            if(rivalChoice===3){
+                winMessage = playMessage[4];
+                winner = 'rival';
+                break;
+            }
+        case 'tijera':
+            if(rivalChoice===1){
+                winMessage = playMessage[7]
+                winner = 'rival';
+                break;
+           }
+            if(rivalChoice===2){
+                winMessage = playMessage[6]
+                break;
+                }
+            if(rivalChoice===3){
+                winMessage = playMessage[0]
+                break;
+                }
+            // console.log(winMessage)
+            return winner;
+    }
+ }
