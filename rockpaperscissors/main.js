225ca36f97a5f655be6eeb0ddf405db39f78c47c
davidchavezc <177746616+davidchavezc@users.noreply.github.com>
2024-12-20 23:37:57 -0600

    Finish functions for rcp

--- rockpaperscissors/main.js
+++ rockpaperscissors/main.js
@@ -1,8 +1,7 @@
+userName = 'David';
 let userScore = 0;
 let rivalScore = 0;
 const buttons = document.querySelectorAll('button');
-const displayUserScore = document.querySelector('#userScore');
-const displayRivalScore = document.querySelector('#rivalScore');
 
 function getComputerChoice(){
     let computerChoice = parseInt(Math.random() * 3) + 1;
@@ -16,9 +15,63 @@ function getComputerChoice(){
     return computerChoice;
 }
 
+function playRound(usChoice, riChoice) {
+    const winningMessage = document.querySelector('#winningMessage');
+    let winner = null;
+    let roundResults;
+    switch(usChoice){
+        case 'rock':
+            if(riChoice ==='rock'){
+                roundResults = 'Draw!!'
+            }
+            else if(riChoice === 'paper'){
+                roundResults = "Your rock is wrapped by the rivals' paper!";
+                winner = 'rival';
+            }
+            else{
+                roundResults = "Your rock destroys the rivals' scissors!"
+                winner = 'user'
+            }
+            break;
+        case 'paper':
+            if(riChoice ==='rock'){
+                roundResults = "Your paper wraps around your rivals' rock!";
+                winner = 'user';
+            }
+            else if(riChoice === 'paper'){
+                roundResults = "Draw";
+            }
+            else{
+                roundResults = "The rivals' scissors slash trough your paper!";
+                winner = 'rival';
+            }
+            break;
+        case 'scissors':
+            if(riChoice ==='rock'){
+                roundResults = "The rivals' rock smashes your scissors!";
+                winner = 'rival';
+            }
+            else if(riChoice === 'paper'){
+                roundResults = "Your scissors cut trough the rivals' paper!";
+                winner = 'user';
+            }
+            else{
+                roundResults = "Draw";
+            }
+    }
+    winningMessage.textContent = roundResults;
+    return winner;
+}
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
+        const scores = document.querySelector('#displayScores');
         userChoice = button.id;
         rivalChoice = getComputerChoice();
+        let winner = playRound(userChoice, rivalChoice);
+        if(winner){
+            winner === 'user' ? userScore++ : rivalScore++; 
+        }
+        scores.textContent = `${userName}: ${userScore} Rival: ${rivalScore}`;
     })
 })
